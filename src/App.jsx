0f82904c66a9f047fdde6b3031b290a57e67e822
Chinmay2390 <chinmaykamlaskar@gmail.com>
2025-03-10 08:12:57 +0530

    added components in which authentication dashboard navbar and home.jsx added

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { useColorMode } from "@chakra-ui/react";
+import Navbar from "./components/Navbar/Navbar";
+import ChatState from "./context/appState";
+import { useContext } from "react";
+import chatContext from "./context/chatContext";
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const { toggleColorMode } = useColorMode();
+  const context = useContext(chatContext);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="app">
+        <Navbar  toggleColorMode={toggleColorMode} context={context} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
+      
   )
 }
 
